ac88e7b3f9527d4703afa34291cf2732a8331236
ParkerN636 <pnguye30@email.cpcc.edu>
2023-09-11 19:53:03 -0400

    Revert "descriptive"
    This reverts commit 397ed03edf3f2a3b9aecfa881926aeca189dd079.

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,6 @@ public class HelloWorld // class header
    public static void main(String[] args) // main method header
    {      
       System.out.println("Hello world!");
-      System.out.println("Program modified, but able to revert to original version using GitHub Desktop.");
       
    }
 }
