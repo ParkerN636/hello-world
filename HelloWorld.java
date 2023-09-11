397ed03edf3f2a3b9aecfa881926aeca189dd079
ParkerN636 <pnguye30@email.cpcc.edu>
2023-09-11 19:49:46 -0400

    descriptive
    Added print "Program modified, but able to revert to original version using GitHub Desktop"

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,7 @@ public class HelloWorld // class header
    public static void main(String[] args) // main method header
    {      
       System.out.println("Hello world!");
+      System.out.println("Program modified, but able to revert to original version using GitHub Desktop.");
       
    }
 }
